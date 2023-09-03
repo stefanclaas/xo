5ebd5d75220323e2dace75f3f00a94b647fd6d52
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-09-03 19:57:11 +0200

    Update xo.go

--- xo.go
+++ xo.go
@@ -64,10 +64,10 @@ func formatGroups(encoded string) string {
 
 	for i, group := range groups {
 		formatted.WriteString(group)
-		if (i+1)%5 == 0 && (i+1)%55 != 0 {
+		if (i+1)%5 == 0 && (i+1)%55 != 0 { // change to %30, if using with exo, for Usenet
 			formatted.WriteString(" ")
 		}
-		if (i+1)%55 == 0 {
+		if (i+1)%55 == 0 { // change to %30, if using with exo, for Usenet
 			formatted.WriteString("\n")
 		}
 	}
